3ba6fbc46d0cbb0097bfaa0a7c320cfd222c806e
DariaMaistrenko <55316805+DaryaMaistrenko@users.noreply.github.com>
2024-08-13 21:06:14 +0200

    architecture

--- models/Phone.py
+++ models/Phone.py
@@ -1,2 +1,5 @@
 class Phone:
-    pass
+    def __init__(self, value):
+     super().__init__(value)
+     if not value.isdigit() or len(value) != 10:
+       raise ValueError("Phone number must be a 10-digit number. Please, provide a correct phone number")
