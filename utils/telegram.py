6b3d1e5a8b17a9995ab54cf8c5961da0efb17d6b
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2024-08-13 18:26:52 +0300

    final code for telegram bot

--- utils/telegram.py
+++ utils/telegram.py
@@ -1,9 +1,12 @@
-import os
 import requests
 from credentials import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 
 
-def send_telegram_message(message, telegram_bot_token=TELEGRAM_BOT_TOKEN, telegram_chat_id=TELEGRAM_CHAT_ID):
+def send_telegram_message(
+        message: str, 
+        telegram_bot_token: str = TELEGRAM_BOT_TOKEN, 
+        telegram_chat_id: str = TELEGRAM_CHAT_ID
+        ):
     url = f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage"
     data = {
         "chat_id": telegram_chat_id,
