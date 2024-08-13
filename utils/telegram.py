f0b64038a1817285c55a635360b4ab0e295c3e9d
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2024-08-13 18:23:14 +0300

    created bot in Bot Father

--- utils/telegram.py
+++ utils/telegram.py
@@ -1,8 +1,9 @@
 import os
 import requests
+from credentials import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 
 
-def send_telegram_message(message, telegram_bot_token=None, telegram_chat_id=None):
+def send_telegram_message(message, telegram_bot_token=TELEGRAM_BOT_TOKEN, telegram_chat_id=TELEGRAM_CHAT_ID):
     url = f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage"
     data = {
         "chat_id": telegram_chat_id,
@@ -10,3 +11,9 @@ def send_telegram_message(message, telegram_bot_token=None, telegram_chat_id=Non
     }
     response = requests.post(url, data=data)
     return response.json()
+
+
+if __name__ == "__main__":
+    message = "Hello, World!"
+    response = send_telegram_message(message)
+    print(response)
