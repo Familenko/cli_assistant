6d1c7fa0e787c9719c0783f6ed53559aa30f5b3f
DariaMaistrenko <55316805+DaryaMaistrenko@users.noreply.github.com>
2024-08-14 10:05:55 +0200

    architecture

--- models/Phone.py
+++ models/Phone.py
@@ -1,5 +1,14 @@
 class Phone:
     def __init__(self, value):
-     super().__init__(value)
-     if not value.isdigit() or len(value) != 10:
-       raise ValueError("Phone number must be a 10-digit number. Please, provide a correct phone number")
+        super().__init__()
+        self.value = self.validate_phone(value)
+    
+    def validate_phone(self, value):
+        # checking "+" symbol
+        if value.startswith('+'):
+            # delete of "+" before checking whether number consists of 10 symbols
+            value = value[1:]
+        if not value.isdigit() or len(value) != 10:
+            raise ValueError("Phone number must be a 10-digit number without the country code or with '+' followed by 10 numbers. Please, provide a correct phone number.")
+        
+        return value
