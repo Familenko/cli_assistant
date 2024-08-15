2657c968512f94fcd91f73ce69cafe9da6faa4de
DariaMaistrenko <55316805+DaryaMaistrenko@users.noreply.github.com>
2024-08-15 15:40:25 +0200

    Update Name.py

--- models/Name.py
+++ models/Name.py
@@ -1,5 +1,6 @@
 class Name:
     def __init__(self, value):
+     super().__init__(value)
      if len(str(value)) < 2:
        raise ValueError("The requested name should consist of at least 2 letters. Please, use a different name")
      else: self.value = str(value).title()
